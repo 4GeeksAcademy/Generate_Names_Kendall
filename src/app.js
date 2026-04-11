bf71124e792a86390074078230c8bfc31f4fc056
Kenube21 <kendallsh51@gmail.com>
2026-04-11 03:17:25 +0000

    first details

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let end = ['.com', '.net', '.us', '.io']
+
+  const domains = [];
+  
+  pronoun.forEach(pr => {
+    adj.forEach(ad => {
+      noun.forEach(no => {
+        end.forEach(en => {
+          domains.push(`${pr}${ad}${no}${en}`)
+        })
+      })
+    })
+  })
+
+  let domTrans = domains.map(n => {
+    return `
+      <li class="list-group-item">${n}</li>
+    `
+  })
+
+  document.getElementById("generate").innerHTML = domTrans.join('\n');
+
 };
+
+
+
