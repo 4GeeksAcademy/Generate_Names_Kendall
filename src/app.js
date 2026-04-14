4c4aa8db14466ec6a69e29e00f20edc6f7537dbe
Kenube21 <kendallsh51@gmail.com>
2026-04-14 00:17:10 +0000

    news_details

--- src/app.js
+++ src/app.js
@@ -7,18 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let end = ['.com', '.net', '.us', '.io']
+  let pronoun = ['el', 'nuestro', 'mi', 'tu'];
+  let adj = ['gran', 'mejor', 'super', 'puro'];
+  let noun = ['puedes', 'ofertas', 'donde', 'vende', 'carro', 'camioneta', 'bebe'];
+  let end = ['.es', '.as', '.de', '.it', '.re'];
 
   const domains = [];
+
+  function createDomainHack (names,extension) {
+    let extClear = extension.replace('.','');
+
+    if (names.endsWith(extClear)) {
+      return names.slice(0 ,-extClear.length);
+    } 
+    return names;
+  }
   
   pronoun.forEach(pr => {
     adj.forEach(ad => {
       noun.forEach(no => {
         end.forEach(en => {
-          domains.push(`${pr}${ad}${no}${en}`)
+
+          domains.push(`${pr}${ad}${createDomainHack(no,en)}${en}`)
         })
       })
     })
